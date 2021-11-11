a06a7473dd2d2f82d7ce5c14fd7b2f0d3f0586b8
Daxxn <nynjalantz@gmail.com>
2021-11-10 20:13:16 -0800

    Re-Fix ipAddress output

--- src/index.js
+++ src/index.js
@@ -139,7 +139,7 @@ const start = () => {
         if (err) {
           console.log(err);
         } else {
-          console.log(`Started Server  @  ${server.address()}`);
+          console.log(`Started Server  @  ${JSON.stringify(server.address())}`);
         }
       });
     }
